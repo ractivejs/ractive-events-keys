4a6b19c2ef1f50ad30a2e0a0df49d8fa00600d13
Rich Harris <richard.a.harris@gmail.com>
2013-12-15 11:56:55 -0500

    initial commit

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// Ractive-events-keys tests
+// ===================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
