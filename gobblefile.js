38a81e3d8bf3e395008f77eec8cc3f3682aec282
Rich-Harris <richard.a.harris@gmail.com>
2015-04-11 17:41:42 -0400

    add minified version to dist/

--- gobblefile.js
+++ gobblefile.js
@@ -2,19 +2,27 @@ var gobble = require( 'gobble' );
 
 gobble.cwd( __dirname );
 
-module.exports = gobble( 'src' )
-.transform( 'babel', {
-	blacklist: [ 'es6.modules', 'useStrict' ],
-	sourceMap: false
-})
-.transform( 'esperanto-bundle', {
-	entry: 'ractive-events-keys',
-	type: 'umd',
-	name: '__PLACEHOLDER__',
-	sourceMap: false,
-	strict: true
-})
-.transform( function ( input ) {
-	// this is an egregious hack, and totally brittle. YOLO
-	return input.replace( '(global.__PLACEHOLDER__ = {})', 'global.Ractive.events' );
-});
+var lib = gobble( 'src' )
+	.transform( 'babel', {
+		blacklist: [ 'es6.modules', 'useStrict' ],
+		sourceMap: false
+	})
+	.transform( 'esperanto-bundle', {
+		entry: 'ractive-events-keys',
+		type: 'umd',
+		name: '__PLACEHOLDER__',
+		sourceMap: false,
+		strict: true
+	})
+	.transform( function ( input ) {
+		// this is an egregious hack, and totally brittle. YOLO
+		return input.replace( '(global.__PLACEHOLDER__ = {})', 'global.Ractive.events' );
+	});
+
+module.exports = gobble([
+	lib,
+	lib.transform( 'uglifyjs', {
+		ext: '.min.js',
+		sourceMap: false
+	})
+]);
